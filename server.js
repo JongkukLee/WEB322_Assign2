357e925ac90c1dc6bb95446e57f3a31bb6ce4f40
Jongkuk Lee <jlee465@sy-t3078-74.local>
2017-05-24 13:06:41 -0400

    .

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: ______________________ Student ID: ______________ Date: ________________
+*
+* Online (Heroku) Link: __https://arcane-fjord-78560herokuapp.com______________________________________________________
+*
+********************************************************************************/
+
 var express = require("express");
 var app = express();
 
